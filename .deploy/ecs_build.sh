f8fb65cb1595d1bf22c4a4b89d409a93fb3fba28
Sushil Shinde <shinde.sushil@gmail.com>
2018-02-15 16:30:19 +0530

    Added ARN role again

--- .deploy/ecs_build.sh
+++ .deploy/ecs_build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 ENV=$1
 if [[ -z "$ENV" ]] ; then
-	echo "Environment should be set on startup with one of the below values"
-	echo "ENV must be one of - DEV, QA, PROD or LOCAL"
-	exit
+  echo "Environment should be set on startup with one of the below values"
+  echo "ENV must be one of - DEV, QA, PROD or LOCAL"
+  exit
 fi
 
 echo "$ENV before case conversion"
@@ -42,102 +42,105 @@ docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASSWD
 #echo "$ENV after case conversion"
 
 configure_aws_cli() {
-	aws --version
-	aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
-	aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
-	aws configure set default.region $AWS_REGION
-	aws configure set default.output json
-	echo "Configured AWS CLI."
+  aws --version
+  aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
+  aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
+  aws configure set default.region $AWS_REGION
+  aws configure set default.output json
+  echo "Configured AWS CLI."
 }
 
 build_ecr_image() {
-	eval $(aws ecr get-login  --region $AWS_REGION)
-	#eval $(aws ecr get-login)
-	# Builds Docker image of the app.
-	#$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$AWS_REPOSITORY:$CIRCLE_SHA1
-	TAG=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$AWS_REPOSITORY:$CIRCLE_SHA1
-	docker build -t $TAG .
+  eval $(aws ecr get-login  --region $AWS_REGION)
+  #eval $(aws ecr get-login)
+  # Builds Docker image of the app.
+  #$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$AWS_REPOSITORY:$CIRCLE_SHA1
+  TAG=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$AWS_REPOSITORY:$CIRCLE_SHA1
+  docker build -t $TAG .
 }
 
-push_ecr_image() {	
-	echo "Pushing Docker Image...."
-	eval $(aws ecr get-login --region $AWS_REGION --no-include-email)
-	echo $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$AWS_REPOSITORY:$TAG
-	docker push $TAG
-	echo "Docker Image published."
+push_ecr_image() {
+  echo "Pushing Docker Image...."
+  eval $(aws ecr get-login --region $AWS_REGION --no-include-email)
+  echo $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$AWS_REPOSITORY:$TAG
+  docker push $TAG
+  echo "Docker Image published."
 }
 
 make_task_def(){
-	task_template='[
-		{
-		"name": "%s",
-		"image": "%s.dkr.ecr.%s.amazonaws.com/%s:%s",
-		"essential": true,
-		"memory": 500,
-		"cpu": 100,
-		"environment": [
+task_template=$(cat <<-END
+{
+  
+  "containerDefinitions": [
+    {
+    "name": "%s",
+    "image": "%s.dkr.ecr.%s.amazonaws.com/%s:%s",
+    "essential": true,
+    "memory": 500,
+    "cpu": 100,
+    "environment": [
         {
           "name": "AUTH_DOMAIN",
-          "value": "%s%"
+          "value": "%s"
         },
         {
           "name": "DOCUSIGN_INTEGRATOR_KEY",
-          "value": "%s%"
+          "value": "%s"
         },
         {
           "name": "DOCUSIGN_NDA_TEMPLATE_ID",
-          "value": "%s%"
+          "value": "%s"
         },
         {
           "name": "DOCUSIGN_PASSWORD",
-          "value": "%s%"
+          "value": "%s"
         },
         {
           "name": "DOCUSIGN_RETURN_URL",
-          "value": "%s%"
+          "value": "%s"
         },
         {
           "name": "DOCUSIGN_SERVER_URL",
-          "value": "%s%"
+          "value": "%s"
         },
         {
           "name": "DOCUSIGN_USERNAME",
-          "value": "%s%"
+          "value": "%s"
         },
         {
           "name": "OLTP_PW",
-          "value": "%s%"
+          "value": "%s"
         },
         {
           "name": "OLTP_URL",
-          "value": "%s%"
+          "value": "%s"
         },
         {
           "name": "OLTP_USER",
-          "value": "%s%"
+          "value": "%s"
         },
         {
           "name": "SMTP_HOST",
-          "value": "%s%"
+          "value": "%s"
         },
         {
           "name": "SMTP_PASSWORD",
-          "value": "%s%"
+          "value": "%s"
         },
         {
           "name": "SMTP_SENDER",
-          "value": "%s%"
+          "value": "%s"
         },
         {
           "name": "SMTP_USERNAME",
-          "value": "%s%"
+          "value": "%s"
         },
         {
           "name": "TC_JWT_KEY",
-          "value": "%s%"
+          "value": "%s"
         }
       ],
-		"portMappings": [
+    "portMappings": [
         {
           "hostPort": 8080,
           "protocol": "tcp",
@@ -149,26 +152,42 @@ make_task_def(){
           "containerPort": 8081
         }
       ],
-		"logConfiguration": {
-			"logDriver": "awslogs",
-				"options": {
-							"awslogs-group": "/ecs/%s",
-							"awslogs-region": "%s",
-							"awslogs-stream-prefix": "ecs"
-				}
-			}
-		}
-	]'
-	echo $AWS_ECS_CONTAINER_NAME $AWS_ACCOUNT_ID $AWS_REGION $AWS_REPOSITORY $TAG "$AUTH_DOMAIN" $DOCUSIGN_INTEGRATOR_KEY $DOCUSIGN_NDA_TEMPLATE_ID $DOCUSIGN_PASSWORD $DOCUSIGN_RETURN_URL $DOCUSIGN_SERVER_URL $DOCUSIGN_USERNAME $OLTP_PW $OLTP_URL $OLTP_USER $SMTP_HOST $SMTP_PASSWORD $SMTP_SENDER $SMTP_USERNAME $TC_JWT_KEY $AWS_ECS_CLUSTER $AWS_REGION
-	task_def=$(printf $task_template $AWS_ECS_CONTAINER_NAME $AWS_ACCOUNT_ID $AWS_REGION $AWS_REPOSITORY $TAG "$AUTH_DOMAIN" $DOCUSIGN_INTEGRATOR_KEY $DOCUSIGN_NDA_TEMPLATE_ID $DOCUSIGN_PASSWORD $DOCUSIGN_RETURN_URL $DOCUSIGN_SERVER_URL $DOCUSIGN_USERNAME $OLTP_PW $OLTP_URL $OLTP_USER $SMTP_HOST $SMTP_PASSWORD $SMTP_SENDER $SMTP_USERNAME $TC_JWT_KEY $AWS_ECS_CLUSTER $AWS_REGION)
-  echo task_def
+    "logConfiguration": {
+      "logDriver": "awslogs",
+        "options": {
+              "awslogs-group": "/ecs/%s",
+              "awslogs-region": "%s",
+              "awslogs-stream-prefix": "ecs"
+        }
+      }
+    }
+  ],
+"family": "%s",
+"requiresCompatibilities": ["FARGATE"],
+"networkMode": "awsvpc"
+}
+END
+)
+  echo "-------- family param value:"
+  echo $family
+  family_val= $family | $($JQ '.taskDefinition.taskDefinitionArn')
+  echo $family_val
+  echo "-------- vars to inject into template:"
+  echo $AWS_ECS_CONTAINER_NAME $AWS_ACCOUNT_ID $AWS_REGION $AWS_REPOSITORY $TAG "$AUTH_DOMAIN" $DOCUSIGN_INTEGRATOR_KEY $DOCUSIGN_NDA_TEMPLATE_ID $DOCUSIGN_PASSWORD $DOCUSIGN_RETURN_URL $DOCUSIGN_SERVER_URL $DOCUSIGN_USERNAME $OLTP_PW $OLTP_URL $OLTP_USER $SMTP_HOST $SMTP_PASSWORD $SMTP_SENDER $SMTP_USERNAME $TC_JWT_KEY $AWS_ECS_CLUSTER $AWS_REGION $family_val
+  echo "-------- template:"
+  echo $task_template
+
+  task_def=$(printf "$task_template" $AWS_ECS_CONTAINER_NAME $AWS_ACCOUNT_ID $AWS_REGION $AWS_REPOSITORY $TAG "$AUTH_DOMAIN" $DOCUSIGN_INTEGRATOR_KEY $DOCUSIGN_NDA_TEMPLATE_ID $DOCUSIGN_PASSWORD $DOCUSIGN_RETURN_URL $DOCUSIGN_SERVER_URL $DOCUSIGN_USERNAME $OLTP_PW $OLTP_URL $OLTP_USER $SMTP_HOST $SMTP_PASSWORD $SMTP_SENDER $SMTP_USERNAME $TC_JWT_KEY $AWS_ECS_CLUSTER $AWS_REGION $family_val)
+  echo "-------- task def after injection:"
+  echo $task_def
+  echo $task_def > config.json
 }
 
 
-register_definition() {  
+register_definition() {
     echo "register definition"
-    echo aws ecs register-task-definition --region "$AWS_REGION"  --container-definitions $task_def --family $family 
-    if revision=$(aws ecs register-task-definition --region "$AWS_REGION"  --container-definitions "$task_def" --family $family | $JQ '.taskDefinition.taskDefinitionArn'); then
+    echo aws ecs register-task-definition --cli-input-json file://config.json --family $family
+    if revision=$(aws ecs register-task-definition --execution-role-arn "arn:aws:iam::811668436784:role/EcsTaskExecutionRole" --task-role-arn "arn:aws:iam::811668436784:role/EcsTaskExecutionRole" --cli-input-json file://config.json --family $family | $JQ '.taskDefinition.taskDefinitionArn'); then
         echo "Revision: $revision"
     else
         echo "Failed to register task definition"
@@ -179,7 +198,7 @@ register_definition() {
 
 check_service_status() {
         counter=0
-	sleep 60
+  sleep 60
         servicestatus=`aws ecs describe-services --service $AWS_ECS_SERVICE --cluster $AWS_ECS_CLUSTER | $JQ '.services[].events[0].message'`
         while [[ $servicestatus != *"steady state"* ]]
         do
